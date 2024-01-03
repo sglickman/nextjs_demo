e00c3c8c84e7c3c2adf7e4813fdbc226fcc4535c
Seth <seth@sethsmacstudio.lan>
2024-01-02 21:22:45 -0500

    fourth commit, fixing error

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,8 +1,3 @@
-import { useSession } from "next-auth/react";
-
 export default async function Page() {
-    const { data: session, status } = useSession()
-    console.log(session);
-    console.log(status);
     return <p>User Settings</p>
 }
